0da5cf4d60a09d789d0db1f5d755635420845352
Stefan Wiedemann <wistefan@googlemail.com>
2024-06-12 07:50:21 +0200

    fix npe

--- openapi/caching_client.go
+++ openapi/caching_client.go
@@ -19,19 +19,19 @@ func NewCachingDocumentLoader(defaultLoader ld.DocumentLoader) ld.DocumentLoader
 		contextCache: cache.New(time.Duration(300)*time.Second, time.Duration(600)*time.Second)}
 }
 
-func (cdl CachingDocumentLoader) LoadDocument(u string) (doc *ld.RemoteDocument, err error) {
+func (cdl CachingDocumentLoader) LoadDocument(u string) (document *ld.RemoteDocument, err error) {
 	logging.Log().Infof("Get the document %s from the loader.", u)
-	document, hit := cdl.contextCache.Get(u)
+	cachedDocument, hit := cdl.contextCache.Get(u)
 	if hit {
 		logging.Log().Infof("Found %s in the cache.", u)
-		return document.(*ld.RemoteDocument), err
+		return cachedDocument.(*ld.RemoteDocument), err
 	}
 	document, err = cdl.defaultLoader.LoadDocument(u)
 	if err != nil {
 		logging.Log().Infof("Was not able to load %s", u)
-		return doc, err
+		return document, err
 	}
 	cdl.contextCache.Set(u, document, cache.DefaultExpiration)
 	logging.Log().Infof("Added %s to the cache.", u)
-	return doc, err
+	return document, err
 }
