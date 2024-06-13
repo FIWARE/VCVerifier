f6add61a5c4e0cde683ead88d1471ba0ab9424d1
Stefan Wiedemann <wistefan@googlemail.com>
2024-06-13 14:29:18 +0200

    logging

--- verifier/trustedissuer.go
+++ verifier/trustedissuer.go
@@ -66,7 +66,11 @@ func (tpvs *TrustedIssuerValidationService) ValidateVC(verifiableCredential *ver
 		exist, trustedIssuer, err := tpvs.tirClient.GetTrustedIssuer(tilAddress, verifiableCredential.Contents().Issuer.ID)
 		var currentTry = 0
 		for currentTry < 3 {
+			if err != nil {
+				logging.Log().Warnf("Error is %v", err)
+			}
 			if err != nil && err.Error() == "EOF" {
+				logging.Log().Warn("Retry")
 				currentTry++
 				exist, trustedIssuer, err = tpvs.tirClient.GetTrustedIssuer(tilAddress, verifiableCredential.Contents().Issuer.ID)
 				continue
