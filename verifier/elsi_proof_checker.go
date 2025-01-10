a134ad0176f1bacd44889f12cc11d3db06ddad80
Stefan Wiedemann <wistefan@googlemail.com>
2025-01-10 16:05:54 +0100

    std

--- verifier/elsi_proof_checker.go
+++ verifier/elsi_proof_checker.go
@@ -61,7 +61,7 @@ func (epc ElsiProofChecker) checkElsiProof(headers jose.Headers, expectedProofIs
 		return err
 	}
 
-	encodedMessage := string(msg[:])
+	encodedMessage := base64.StdEncoding.EncodeToString(msg)
 	logging.Log().Warnf("Encoded message %s", encodedMessage)
 	encodedSignature := base64.RawURLEncoding.EncodeToString(signature)
 	originalJwt := encodedMessage + "." + encodedSignature
